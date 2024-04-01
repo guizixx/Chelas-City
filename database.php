1d4bae9561ee24b578efa002d053db2e9fadd70b
lks <lisheng02091@gmail.com>
2024-04-01 17:10:38 +0100

    ..

--- database.php
+++ database.php
@@ -7,6 +7,7 @@
 
     try{
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+        mysqli_set_charset($conn, "utf8");
     }
     catch(mysqli_sql_exception){
         echo "Could not connect! <br>";
